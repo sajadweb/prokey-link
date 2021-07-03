6a2a6de84e0f26a8cc975393a6254971f705073b
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-07-03 20:13:41 +0800

    add webcore

--- website/src/js/index.ts
+++ website/src/js/index.ts
@@ -1,8 +1,10 @@
 import * as $ from "jquery"
+import { Device } from "@prokey-io/webcore"
 
-function onConnect(): void 
-{
+function onConnect(): void {    
+    var dev = new Device();
+    dev.TransportConnect();
     console.log("clicked");
-}
+} 
 
-$("#connect_button").on("click", onConnect);
+$("#connect_button").on("click", onConnect);  
