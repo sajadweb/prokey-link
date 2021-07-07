db31ad1cdcc66d9f30c3dc5dc568fd799f8df1e4
Ali Akbar Mohammadi <cyrusgp@gmail.com>
2021-07-07 15:46:41 +0800

    minor fixes

--- library/src/ProkeyLink.ts
+++ library/src/ProkeyLink.ts
@@ -1,11 +1,13 @@
 export class ProkeyLink {
-  private _window: Window | null = null;
+    private _window: Window | null = null;
 
-  /// This function opens the prokey-link website
-  async Connect() {
-    const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-                    width=0,height=0,left=-1000,top=-1000`;
+    /// This function opens the prokey-link website
+    async Connect() {
+        if (this._window == null) {
+            const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+                            width=0,height=0,left=-1000,top=-1000`;
 
-    window.open('http://localhost:3000', 'prokey-link', params);
-  }
+            this._window = window.open('http://localhost:3000', 'prokey-link', params);
+        }
+    }
 }
